f56c3512854a8996d3159f5ddc275537dca82c86
EC2 Default User <ec2-user@ip-172-31-68-204.ec2.internal>
2022-10-28 04:05:19 +0000

    added some parameter checking and a check for existance

--- gdown.sh
+++ gdown.sh
@@ -1,7 +1,19 @@
-# helper scription to do the docker run
+#!/bin/bash
+
+# helper script to do the docker run for gdown utility
 #
 # First parameter is the google drive ID for the file you want to download
 # Second parameter is the destination drive for the file
 # Third parameter is the file name
+
+if [ "$#" -ne 3 ]; then
+    echo "Script requires exactly 3 arguments"
+    exit
+fi
+
+if [ -f "$2/$3" ]; then
+    echo "File $2/$3 already exists. Halting before download"
+    exit
+fi
 
 docker run --volume $2:/tmp/down -it wlaforest/gdown gdown $1 -O /tmp/down/$3
